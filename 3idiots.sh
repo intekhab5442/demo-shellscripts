fc85704263063e8b2421ce317d213444a1bada10
Ubuntu <ubuntu@ip-172-31-37-164.ap-south-1.compute.internal>
2026-05-23 09:35:37 +0000

    added 3idits script

---
+++ 3idiots.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#user define Variable
+hero="rancho"
+villian="virus"
+
+echo "3 idiots ka hero hai $hero"
+
+echo "3 idiots ka villian hai $villian"
+
+#shell/ Environment Variable
+
+echo "current logged in user $USER"
+
+#user input varaiables
+read -p "Rancho ka pura naam kya tha? " fullname
+
+echo "Rancho ka poora naam $fullname tha"
+
+# Arguments
+
+
